6eb6b81ac2c667e2bd1babfdc18b1ec1bb3c69b3
Lejla1502 <lejlasp@hotmail.com>
2020-07-25 01:38:57 +0200

    138 (Implementirati pretragu po vrsti i boji)

--- eNamjestaj.UnitTest/IB120060_UnitTests.cs
+++ eNamjestaj.UnitTest/IB120060_UnitTests.cs
@@ -7,7 +7,7 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System.Collections.Generic;
 using System.Linq;
 using Microsoft.EntityFrameworkCore;
-
+using System;
 
 namespace eNamjestaj.UnitTest
 {
@@ -89,14 +89,61 @@ namespace eNamjestaj.UnitTest
 
         ProizvodiController pc = new ProizvodiController();
         [TestMethod]
+        //[DataRow(null,null)]
+        //[DataRow(1,null)]
+        //[DataRow(2,null)]
+        //[DataRow(5,null)]
+        //[DataRow(null,2)]
+
+        //Testni podaci su samo za bazu na serveru
+        //jer se ID-evi za proizvode ne poklapaju sa onima u lokalnoj
+        //bazi
+        //Na lokalnoj bazi ID-evi se krecu od 17-35
+        //a na app.fit.ba od 1 do 16
         public void Test_ListaProizvoda()
         {
+            var random = new Random();
+            int randomVrstaID = random.Next(0, 5);
+            int randomBojaID = random.Next(0, 5);
+            int? vrstaID = null;
+            int? bojaID = null;
 
-            List<Proizvod> ocekivani = _context.Proizvod.ToList();
-            ViewResult vr = pc.Index(null,null) as ViewResult;
+            if (randomVrstaID != 0)
+                vrstaID = randomVrstaID;
+            if (randomBojaID != 0)
+                bojaID = randomBojaID;
+
+
+            List<Proizvod> ocekivani = new List<Proizvod>();
+
+            if (vrstaID == null && bojaID == null)
+                ocekivani = _context.Proizvod.ToList();
+            else
+               ocekivani= _context.Proizvod.Where(x => x.VrstaProizvodaId == vrstaID && bojaID == null ||
+                                     ((x.ProizvodBojas.Any(pb => pb.BojaId == bojaID) && vrstaID == null) ||
+                                     (x.VrstaProizvodaId == vrstaID && x.ProizvodBojas.Any(pb => pb.BojaId == bojaID)))
+                                    ).ToList();
+
+            ViewResult vr = pc.Index(vrstaID,bojaID) as ViewResult;
             ProizvodiIndexVM aktuelni = vr.Model as ProizvodiIndexVM;
+            List<Proizvod> aProizvodi = new List<Proizvod>();
+            for (int i=0;i< aktuelni.Proizvodi.Count;i++)
+            {
+                Proizvod p = new Proizvod();
+                p.Id = aktuelni.Proizvodi[i].Id;
+                p.Cijena = aktuelni.Proizvodi[i].Cijena;
+                p.Naziv = aktuelni.Proizvodi[i].Naziv;
+                p.Sifra = aktuelni.Proizvodi[i].Sifra;
+                p.Slika = aktuelni.Proizvodi[i].Slika;
+                aProizvodi.Add(p);
+               
+            }
+
             
             Assert.AreEqual(ocekivani.Count, aktuelni.Proizvodi.Count);
+            CollectionAssert.AreEqual(ocekivani,aProizvodi, 
+                                      Comparer<Proizvod>.Create((prvi,drugi)=>
+                                      prvi.Id==drugi.Id && prvi.Sifra==drugi.Sifra?0:1));
         }
 
 
