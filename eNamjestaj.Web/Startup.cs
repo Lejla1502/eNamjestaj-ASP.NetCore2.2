7c332df2d99ae0e2cf0f95008e8ca1eb3dd722e3
Lejla1502 <lejlasp@hotmail.com>
2020-09-06 10:14:08 +0200

    68

--- eNamjestaj.Web/Startup.cs
+++ eNamjestaj.Web/Startup.cs
@@ -63,6 +63,12 @@ namespace eNamjestaj.Web
             app.UseMvc(routes =>
             {
                 routes.MapRoute(
+                   name: "areas",
+                   template: "{area:exists}/{controller=Home}/{action=Index}/{id?}"
+               );
+
+
+                routes.MapRoute(
                     name: "default",
                     template: "{controller=Home}/{action=Index}/{id?}");
             });
